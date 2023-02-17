e1a11f384d57b40aeec419ee2c62f4fdf54271df
akashy7461 <akashy7461@gmail.com>
2023-02-17 14:24:31 +0530

    Create 01_typeOf.js

---
+++ 01_typeOf.js
@@ -0,0 +1,17 @@
+var bankName;
+console.log("*******************1*******************");
+console.log("Before Initilization:  "+ typeof bankName);
+bankName="SBI Bank";
+console.log("After  Initilization:  "+ typeof bankName);
+console.log("*******************2*******************");
+var mark='90%';
+console.log("Data type: "+ typeof mark)
+console.log("*******************3*******************");
+var isWorking;
+console.log("Before Initilization:  "+ typeof isWorking);
+isWorking=true;
+console.log("After Initilization:  "+ typeof isWorking);
+console.log("*******************4*******************");
+var totalCount="hundred and seven";
+console.log("Type of data:  "+typeof totalCount);
+console.log("**************************************");
