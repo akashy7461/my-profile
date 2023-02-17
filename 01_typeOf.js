aa179a44d052caa92347252c5dd621bce32886e8
akashy7461 <akashy7461@gmail.com>
2023-02-17 22:22:37 +0530

    Update 01_typeOf.js

--- 01_typeOf.js
+++ 01_typeOf.js
@@ -15,3 +15,18 @@ console.log("*******************4*******************");
 var totalCount="hundred and seven";
 console.log("Type of data:  "+typeof totalCount);
 console.log("**************************************");
+
+var pin=413305;
+console.log("Pin: ",pin,"type: ",typeof pin);
+
+var str="Hello world";
+console.log("Pin: ",str,"type: ",typeof str);
+
+var con=true;
+console.log("Pin: ",con,"type: ",typeof con);
+
+/*
+diffrence bteween null and undefined
+null: null value means that there is no value. it is not equlvalent to an empty string.
+undefined: a variable is declare and not assigned any value.it is undefined.
+*/
