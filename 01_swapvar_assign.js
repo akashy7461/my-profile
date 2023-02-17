fd8ed2e95605500725359bb2fb1f76a7d9ade41e
akashy7461 <akashy7461@gmail.com>
2023-02-17 22:53:44 +0530

    swaping variable
    swaping two and three variable

---
+++ 01_swapvar_assign.js
@@ -0,0 +1,27 @@
+var sweety="Sweety";
+var cutie="Cutie";
+var temp;
+console.log("before value of variable sweety: ",sweety);
+console.log("before value of variable cutie:",cutie);
+temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("after value of variable sweety: ",sweety);
+console.log("after value of variable cutie:",cutie);
+
+console.log("*********************************");
+/* second question swaping three number */
+var num1=100;
+var num2=200;
+var num3=300;
+var temp1;
+console.log("before value of variable num1: ",num1);
+console.log("before value of variable num2: ",num2);
+console.log("before value of variable num3: ",num3);
+temp1=num1;
+num1=num2;
+num2=num3;
+num3=temp1;
+console.log("after value of variable num1: ",num1);
+console.log("after value of variable num2: ",num2);
+console.log("after value of variable num3: ",num3);
