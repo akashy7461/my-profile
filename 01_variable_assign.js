8529d9ff58c25a5d3ad6b3ccfe8a44e5955a1e71
akashy7461 <akashy7461@gmail.com>
2023-02-17 14:12:25 +0530

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+var myFullName='Akash Yadav'
+console.log(myFullName)
+var myFullName='Akash Shivaji Yadav'
+console.log(myFullName)
+
+var myLove
+myLove='I love only JS'
+console.log(myLove)
+
+var firstName,lastName,age,address,pin_code,gender,is_married
+firstName='akash'
+lastName='yadav'
+age=22
+address='Marapur'
+pin_code=413305
+gender='Male'
+is_married='single'
+console.log(firstName,lastName,age,address,pin_code,gender,is_married)
